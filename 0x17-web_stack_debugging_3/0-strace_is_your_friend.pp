86e7621bb9608f617b1e42e26ce4f7c23c4b6d04
Sherry680 <faithnjer506@gmail.com>
2023-05-12 16:03:59 +0000

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,7 @@
 
 
 exec{ 'fix-wordpress':
-    command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/'
-    }
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
+
